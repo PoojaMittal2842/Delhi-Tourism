7c20084260d198ca0f6def8a5b0d0849276cdedb
kirthanaa <kirthana.s@btech.christuniversity.in>
2021-03-26 00:45:44 +0530

    Fixed search button and added filter

--- Assets/js/browse_place.js
+++ Assets/js/browse_place.js
@@ -18,20 +18,22 @@ window.onload = () => {
   });
 
 
-
   function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
     }
-} 
+  }
