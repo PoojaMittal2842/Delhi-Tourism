c900afb79377c06d781f2b43f4ec75a199f5e726
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-03-29 03:49:52 +0530

    Update Book_ticket.js

--- Assets/js/Book_ticket.js
+++ Assets/js/Book_ticket.js
@@ -30,3 +30,36 @@ function validate() {
     }
 
 }
+
+const toggleSwitch=document.querySelector('.togglebtn');
+        const text=$('#light-dark-text');
+        const icon=$('#light-dark-icon');
+        function darkMode(){
+            text.text("Dark");
+            icon.attr('class','fa fa-moon-o');
+        }
+        function lightMode() {
+            text.text("Light");
+            icon.attr('class','fa fa-sun-o');
+        }
+        function switchTheme(event) {
+        if (event.target.checked) {
+            document.documentElement.setAttribute("data-theme", "dark");
+            localStorage.setItem("theme", "dark");
+            darkMode();
+        } else {
+            document.documentElement.setAttribute("data-theme", "light");
+            localStorage.setItem("theme", "light");
+            lightMode();
+        }
+        }
+        toggleSwitch.addEventListener("change", switchTheme);
+        const currentTheme = localStorage.getItem("theme");
+        if (currentTheme) {
+        document.documentElement.setAttribute("data-theme", currentTheme);
+
+        if (currentTheme === "dark") {
+            toggleSwitch.checked = true;
+            darkMode();
+        }
+        }
