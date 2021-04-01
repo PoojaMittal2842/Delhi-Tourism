343350e00fa210899c7efbc452e3e28c36ab8aa0
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-04-01 11:17:06 +0530

    Delete Assets directory

--- Assets/js/home.js
+++
@@ -1,8 +0,0 @@
-let btns = document.querySelector('.btn');
-let inputs = document.querySelector('.input');
-let search = document.querySelector('.search');
-
-btns.addEventListener('click', () => {
-    search.classList.toggle('active');
-    inputs.focus();
-})
