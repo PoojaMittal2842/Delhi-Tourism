97aefa3e342a57dc9b18cc286f71c663dbdb971e
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-04-12 00:17:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,166 @@
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const request = require('request');
+const passport = require('passport');    // to authenticate
+const LocalStrategy = require('passport-local');  // to authenticate
+const User = require('./models/user');
+
+
+const app = express();
+
+const { MongoStore } = require('connect-mongo');
+
+const MongoDBStore=require('connect-mongo')(session);
+const dbUrl='mongodb://localhost:27017/delhi-tour';
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+const secret= process.env.SECRET || 'delhi-tourism';
+
+const store=new MongoDBStore({
+    url:dbUrl,
+    secret,
+    touchAfter:24*60*60
+});
+store.on("error",function(e){
+    console.log("Session store error",e);
+});
+
+const sessionConfig = {
+    store,
+    name:'session',
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());   // to store the user in some way
+passport.deserializeUser(User.deserializeUser());  // to store the user in alternate way
+
+app.use((req, res, next) => {
+    console.log(req.session)
+    res.locals.currentUser = req.user; //to have that user then flash it
+    next();
+})
+
+
+
+
+app.get('/', async(req, res) => {
+   
+    if(req.user == undefined)
+    {
+        res.render('User/index');
+    }
+    else{
+        const user=await User.findById(req.user._id);
+        res.render('User/index',{user})
+    }
+    
+});
+
+app.get('/admin', (req, res) => {
+    res.render('Admin/index');
+});
+
+app.get('/addnewplace', (req, res) => {
+    res.render('User/add_new_place');
+});
+
+app.get('/bookticket', (req, res) => {
+    res.render('User/Book_Ticket');
+});
+
+app.get('/browseplace', (req, res) => {
+    res.render('User/Browse_Place');
+});
+
+app.get('/category', (req, res) => {
+    res.render('User/Category');
+});
+
+app.get('/changepassword', (req, res) => {
+    res.render('User/changepassword');
+});
+
+app.get('/contactus', (req, res) => {
+    res.render('User/contactus');
+});
+
+app.get('/login', (req, res) => {
+    res.render('User/login');
+});
+
+app.post("/login",passport.authenticate("local",{
+    failureFlash: true,
+    successRedirect:"/",
+    failureRedirect:"/login"
+}),function(req,res){
+
+});
+
+app.get('/registration', (req, res) => {
+    res.render('User/registration');
+});
+
+app.post('/registration', (req, res) => {
+    User.register(new User({username:req.body.username,firstname:req.body.firstname,lastname: req.body.lastname,country:req.body.country,contact:req.body.contact}),req.body.password,function(err,user){
+        if(err){
+        return res.render('User/registration',{'error':err.message});
+    }
+        passport.authenticate("local")(req,res,function(){
+            res.redirect('/');
+        });
+    });
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    req.session.destroy(function (err) {
+        res.redirect('/login'); //Inside a callback… bulletproof!
+    });
+});
+
+app.get('/userprofile', (req, res) => {
+    res.render('User/User_Profile');
+});
+
+
+const port=process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
+})
