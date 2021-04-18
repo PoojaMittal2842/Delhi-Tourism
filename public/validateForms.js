65599f2649643350869526aa519896fe1b75a278
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-04-18 23:59:33 +0530

    Add files via upload

---
+++ public/validateForms.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict'
+    bsCustomFileInput.init()
+    const forms = document.querySelectorAll('.validated-form')
+    Array.from(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+})();
