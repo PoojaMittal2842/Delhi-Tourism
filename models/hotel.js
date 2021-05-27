404abc029c12bb57e3eb009158f55ebd405c3b06
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-05-27 16:46:40 +0530

    Update hotel.js

--- models/hotel.js
+++ models/hotel.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
+const HotelSchema = new Schema({
+    author:String,
+    firstname:String,
+    lastname:String,
+    contact:String,
+    email:String,
+    people:String,
+    type:String,
+    checkin:String,
+    checkout:String
+});
+
+
+module.exports = mongoose.model('Hotel', HotelSchema);
